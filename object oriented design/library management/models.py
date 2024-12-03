6bc72b990777808de6642effacae7f2d052033a6
aaronlin <aaron.lin.2@stonybrook.edu>
2024-12-03 17:34:48 -0500

    Implemented Enum Models

--- object oriented design/library management/models.py
+++ object oriented design/library management/models.py
@@ -1,16 +1,33 @@
 from enum import Enum
 
 class BookFormat(Enum):
-    pass
+    HARDCOVER = 1
+    PAPERBACK = 2
+    AUDIOBOOK = 3
+    EBOOK = 4
+    NEWSPAPER = 5
+    MAGAZINE = 6
+    JOURNAL = 7
 
 class BookStatu(Enum):
-    pass
+    AVAILABLE = 1
+    RESERVED = 2
+    LOANED = 3
+    LOST = 4
 
 class ReservationStatus(Enum):
-    pass
+    WAITING = 1
+    PENDING = 2
+    COMPLETED = 3
+    CANCELLED = 4
+    NONE = 5
 
 class AccountStatus(Enum):
-    pass
+    ACTIVE = 1
+    CLOSED = 2
+    CANCELLED = 3
+    BLACKLISTED = 4
+    NONE = 5
 
 class Address:
     def __init__(self,
